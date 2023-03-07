de059fabaadaedb6a606018f7c1f6992386a5e6a
kvnguyen19 <kerannguyen19@gmail.com>
2023-03-07 05:17:28 -0600

    Homework 3 Final

--- source/data_preprocess.py
+++ source/data_preprocess.py
@@ -5,11 +5,13 @@ class DataPreprocessing():
     def __init__(self):
         pass
 
-    def load_data(self, path):
+    def load_data(path):
         data = pd.read_csv(path, sep="\t", header=None).dropna()
 
-        print(data.head())
+        print(data.head()) 
 
         data = data.to_numpy()
 
         return data
+
+    data = load_data('/Users/Nguyener/Documents/GitHub/meters/data/Meter_A.txt')
